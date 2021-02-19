e237877b0100fced6fb70be6648fa81955863d75
mcmomo <moritz@bantleon.me>
2021-02-19 10:56:28 +0100

    🎁 created svelte project

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Navigation from './Components/Navigation.svelte'
+	export let name
+</script>
+
+<main>
+	<Navigation />
+	<div class='background' />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 0;
+		max-width: 240px;
+		margin: 0 auto;
+
+		height: 100%;
+
+		background: linear-gradient(14deg, transparent 50%, var(--beige) 50%);
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.background {
+		position: absolute;
+		height: 100%;
+		width: 100%;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Components/Navigation.svelte
@@ -0,0 +1,18 @@
+<nav class='navigation'>
+	<a href='/'><img src='../assets/icons/grabyourshit-icon-04.svg' alt='Pfote in Tüte'/></a>
+	<slot></slot>
+</nav>
+
+<style type="text/scss">
+	.navigation {
+		display: flex;
+		flex-direction: row;
+		place-content: center;
+	
+		height: 4em;
+
+		img {
+			height: 4em;
+		}
+	}
+</style>
